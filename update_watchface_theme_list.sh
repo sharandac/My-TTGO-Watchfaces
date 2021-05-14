50f928ab66ffcd09da80754c124016eddad7a794
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2021-05-14 21:27:05 +0200

    Sort watchfaces by name
    The result returned by GitHub API seems not sorted consistently.
    Sorting them allow to have always the same order.

--- update_watchface_theme_list.sh
+++ update_watchface_theme_list.sh
@@ -9,4 +9,4 @@ github_repos()
   curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/search/repositories?q=topic:my-ttgo-watch-face | \
   jq '[.items[] | {name: .name, url: ("https://raw.githubusercontent.com/" + .full_name + "/" + .default_branch), download: (.html_url + "/releases/latest/download/watchface.tar.gz")}]'
 }
-github_repos | jq --tab -s '.[0] + .[1]' local_watchface_theme_list.json - > watchface_theme_list.json
+github_repos | jq --tab -s '(.[0] + .[1]) | sort_by(.name)' local_watchface_theme_list.json - > watchface_theme_list.json
