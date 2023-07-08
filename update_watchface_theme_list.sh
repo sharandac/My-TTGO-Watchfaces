fbaf1134f74ed3c904ee99c201d5fa267781baf3
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2023-07-08 16:15:16 +0000

    Add PGNetHun watchfaces source

--- update_watchface_theme_list.sh
+++ update_watchface_theme_list.sh
@@ -9,4 +9,5 @@ github_repos()
   curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/search/repositories?q=topic:my-ttgo-watch-face | \
   jq '[.items[] | {name: .name, home_url: ("https://github.com/" + .full_name), url: ("https://raw.githubusercontent.com/" + .full_name + "/" + .default_branch), download: (.html_url + "/releases/latest/download/watchface.tar.gz")}]'
 }
-github_repos | jq --tab -s '(.[0] + .[1]) | sort_by(.name)' local_watchface_theme_list.json - > watchface_theme_list.json
+curl https://raw.githubusercontent.com/PGNetHun/PG-TTGO-Watchfaces/main/watchface_theme_list.json > PGNetHun.json
+github_repos | jq --tab -s '(.[0] + .[1] + .[2]) | sort_by(.name)' local_watchface_theme_list.json PGNetHun.json - > watchface_theme_list.json
